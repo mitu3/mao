64da25c13c47c9cd71768700e5243f58ed36c3e4
hongbo <hongbo.tan@bianxianmao.com>
2018-03-05 19:32:45 +0800

    rt6u有

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-from selenium import webdriver
+import requests
 
 
-driver = webdriver.Chrome()
-driver.get("http://www.baidu.com")
+def geturl():
+    url = 'http://mvip.piping.mogumiao.com/proxy/api/get_ip_bs?' \
+          'appKey=09b821b7de08402d9c897262084ba0fd&count={}&expiryDate=0&format=1'.format(1)
+
+    req = requests.get(url).json()
+    return req['msg'][0]['ip']+":"+req['msg'][0]['port']
+
+url1 = geturl()
+print(url1)
